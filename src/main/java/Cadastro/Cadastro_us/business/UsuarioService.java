511e10e20b72d318d5d3b7d4c125ea77e9172941
W Gabriel <gabrielborgesw2@gmail.com>
2025-11-30 18:20:46 -0300

    second

--- src/main/java/Cadastro/Cadastro_us/business/UsuarioService.java
+++ src/main/java/Cadastro/Cadastro_us/business/UsuarioService.java
@@ -1,8 +1,35 @@
 package Cadastro.Cadastro_us.business;
 
+import Cadastro.Cadastro_us.infrastructure.entitys.Usuario;
+import Cadastro.Cadastro_us.infrastructure.repository.UsuarioRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsuarioService {
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    public void salvarUsuario(Usuario usuario) {
+        usuarioRepository.saveAndFlush(usuario);
+    }
+
+    public void buscarUsuarioPorId(Integer id) {
+        usuarioRepository.findById(id);
+    }
+
+    public Usuario buscarUsuarioPorEmail(String email) {
+        return usuarioRepository.findByEmail(email).orElseThrow(
+            () -> new RuntimeException("Usuário não encontrado!"
+        ));
+    }
+
+
+
+    public void deletarUsuarioPorId(Integer id) {
+        usuarioRepository.deleteById(id);
+    }
 
 }
--- src/main/java/Cadastro/Cadastro_us/infrastructure/repository/UsuarioRepository.java
+++ src/main/java/Cadastro/Cadastro_us/infrastructure/repository/UsuarioRepository.java
@@ -3,6 +3,9 @@ package Cadastro.Cadastro_us.infrastructure.repository;
 import Cadastro.Cadastro_us.infrastructure.entitys.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
 
+    Optional<Usuario> findByEmail(String email);
 }
